0d71ade06195fa038e885583c5b0743493ffeb5a
Rodrigo <rodrigoeamarante@gmail.com>
2026-04-10 19:23:34 -0300

    fix cosmeticos

--- js/27_cosmetics.js
+++ js/27_cosmetics.js
@@ -9,6 +9,8 @@ const NICK_STYLES = [
     { id: 'nt-silver', name: 'Plata Pulida', class: 'nt-silver' },
     { id: 'nt-bronze', name: 'Bronce Antiguo', class: 'nt-bronze' },
     { id: 'nt-spark', name: 'Relámpago', class: 'nt-spark' },
+    { id: 'nt-fire', name: 'Fuego Eterno', class: 'nt-fire' },
+    { id: 'nt-water', name: 'Marea Azul', class: 'nt-water' },
     { id: 'nt-dark', name: 'Sombra Abisal', class: 'nt-dark' },
     { id: 'nt-royal', name: 'Realeza', class: 'nt-royal' },
     { id: 'nt-ghost', name: 'Espectral', class: 'nt-ghost' }
@@ -16,12 +18,13 @@ const NICK_STYLES = [
 
 const AVATAR_STYLES = [
     { id: null, name: 'Sin Borde', class: '' },
-    { id: 'av-aura', name: 'Aura Celeste', class: 'av-aura' },
-    { id: 'av-flame', name: 'Fuego Infernal', class: 'av-flame' },
+    { id: 'av-water', name: 'Aura Celeste', class: 'av-water' },
+    { id: 'av-fire', name: 'Fuego Infernal', class: 'av-fire' },
     { id: 'av-ice', name: 'Hielo Ártico', class: 'av-ice' },
     { id: 'av-dragon', name: 'Furia Dragón', class: 'av-dragon' },
     { id: 'av-legend', name: 'Resplandor Legendario', class: 'av-legend' },
-    { id: 'av-master', name: 'Maestro Definitivo', class: 'av-master' }
+    { id: 'av-master', name: 'Maestro Definitivo', class: 'av-master' },
+    { id: 'av-ghost', name: 'Neblina Espectral', class: 'av-ghost' }
 ];
 
 function openProfileEditor() {
